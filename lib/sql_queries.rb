a52dc5ad2601cb088cd394740f03598245c38b5c
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-12 23:50:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "Select projects.title, Sum(pledges.amount) From projects Inner Join pledges
-   On projects.id = pledges.id Sort By projects.title"
+   On projects.id = pledges.id Group by projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
