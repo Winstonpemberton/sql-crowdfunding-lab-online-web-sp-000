216cb4b964260cb814ee30761e46e269eed8f555
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-12 23:34:03 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,4 @@
 Create Table projects (id INTEGER PRIMARY KEY, title Text, category Text, funding_goal Integer,
 start_date Integer, end_date Integer);
+
+Create Table users (id INTEGER PRIMARY KEY, name Text, age Integer);
