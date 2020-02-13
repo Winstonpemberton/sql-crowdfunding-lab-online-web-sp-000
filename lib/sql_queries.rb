bb8791d5d1515b38f40d009a09ae70667c7869fa
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-13 00:07:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,9 +18,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Select projects.title, Sum(pledges.amount) - projects.funding_goal As Over_goal 
-    From projects 
-  Inner Join pledges On projects.id = pledges.project_id 
+  "Select projects.title, Sum(pledges.amount) - projects.funding_goal As Over_goal
+    From projects
+  Inner Join pledges On projects.id = pledges.project_id
     Group by projects.title Having Sum(pledges.amount) >= projects.funding_goal;"
 end
 
